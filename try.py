ca0fed131cdf29b01f70f0a5dfd044a51114ecfd
CodexParas <parasgupta8sep@gmail.com>
2023-10-16 15:31:03 +0530

    Cracked News Insertion

--- try.py
+++ try.py
@@ -1,14 +1,4 @@
 import requests
-
-# import django
-# from django.conf import settings
-# from Blog import Blog_defaults
-
-# settings.configure(default_settings=myapp_defaults, DEBUG=True)
-# django.setup()
-from pathlib import Path
-
-from django.conf import settings
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Blog.settings')
 import django
@@ -24,11 +14,12 @@ from django.contrib.auth.models import User
 user = User.objects.get(username="parasgupta")
 #user="parasgupta"
 title="try"
+slug=title.replace(' ','-')
 content="DEMO"
 image="blog/greenland_01a.png"
 
 blog_obj = BlogModel.objects.create(
             user=user, title=title,
-            content=content, image=image
+            content=content, image=image, slug=slug
         )
 blog_obj.save()
