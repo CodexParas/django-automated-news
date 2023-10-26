8e5ab86726dc8b999273600087a1414db001f789
CodexParas <parasgupta8sep@gmail.com>
2023-10-27 00:33:19 +0530

    Cleaned

--- Blog/settings.py
+++ Blog/settings.py
@@ -138,21 +138,7 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_USE_TLS = True
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'email'
-# EMAIL_HOST_PASSWORD = 'password'
-
 CRONTAB_COMMAND_SUFFIX = '2>&1'
 CRONJOBS = [
 ('1 * * * *', 'home.cron.task')
 ]
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ckabhijeet@gmail.com'
-EMAIL_HOST_PASSWORD = 'Hello@citykonnect123'
